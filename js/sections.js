80dbbf9db8d3e2b97629fd92febd8fa5613cc5e8
John Alexis Guerra Gómez <john.guerra@gmail.com>
2016-10-24 08:43:39 -0400

    Trying to make partial animations to work

--- js/sections.js
+++ js/sections.js
@@ -5,9 +5,10 @@
  * http://bost.ocks.org/mike/chart/
  */
 var scrollVis = function() {
+  "use strict";
   // constants to define the size
   // and margins of the vis area.
-  var width = 600;
+  var width = 700;
   var height = 520;
   var margin = {top:0, left:20, bottom:40, right:10};
 
@@ -31,22 +32,40 @@ var scrollVis = function() {
   // The force simulation
   var simulation = null;
 
+  // A cluster nest for more efficiently drawing the clusters
+  var clusters = null;
+
   // Node's radius
-  var radius = 10;
+  var radius = 7;
 
   // Colors for clusters
   var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+  // Should we show images
+  var HIDE_IMAGES = false;
+
+  // List of nodes to be grayedOut
+  var grayingOutList = [];
+  var grayedOutList = [];
+
+
+  // Variable to control how much should the nodes be grayed out
+  var grayedLevel = 1.0;
+  var grayedScale = d3.scaleLinear()
+    .domain([0, 1.0])
+    .range([1.0, 0.3]);
+
 
 
   // When scrolling to a new section
   // the activation function for that
   // section is called.
-  var activateFunctions = [];
+  var activateFunctions = {};
   // If a section has an update function
   // then it is called while scrolling
   // through the section with the current
-  // progress through the section.
-  var updateFunctions = [];
+  //  through the section.
+  var updateFunctions = {};
 
   /**
    * chart
@@ -60,11 +79,11 @@ var scrollVis = function() {
 
       //Load Data
       setupVis(graph);
-      loadImages(graph)
+      setupData(graph)
 
       // create svg and give it a width and height
-      canvasEle = d3.select(this).selectAll("canvas").data([graph]);
-      var svgEnter = canvasEle.enter().append("canvas");
+      var canvasEle = d3.select(this).selectAll("canvas").data([graph]);
+      canvasEle.enter().append("canvas");
 
       canvas = document.querySelector("canvas");
       context = canvas.getContext("2d");
@@ -77,8 +96,8 @@ var scrollVis = function() {
           // .force("charge", d3.forceManyBody().strength(-1))
           //Better than forceCenter because I can control the strength
 
-          .force("x", d3.forceX(width/2).strength(0.1))
-          .force("y", d3.forceY(height/2).strength(0.1))
+          .force("x", d3.forceX(width/2).strength(0.03))
+          .force("y", d3.forceY(height/2).strength(0.03))
           // .force("center", d3.forceCenter(width / 2, height / 2))
           .force("collide", d3.forceCollide(radius+1).iterations(4))
           // .force("forceX", d3.forceCenter(width / 2, height / 2));
@@ -91,17 +110,16 @@ var scrollVis = function() {
       console.log("Clustering");
       // netClustering.cluster(graph.nodes, graph.links);
       console.log("done");
-      var clusters = d3.nest()
-        .key(function(d) { return d.cluster; })
-        .entries(graph.nodes)
-        .sort(function(a, b) { return b.values.length - a.values.length; });
+
+      //Initialize without nodes
+      updateNodes([]);
+
+
 
       simulation
-          .nodes(graph.nodes)
           .on("tick", ticked);
 
-      // simulation.force("link")
-      //     .links(graph.links);
+
 
       d3.select(canvas)
           .call(d3.drag()
@@ -121,12 +139,15 @@ var scrollVis = function() {
         context.save();
         // context.translate(width / 2, height / 2);
 
-
         context.beginPath();
-        graph.links.forEach(drawLink);
-        context.strokeStyle = 'rgba(200,200,200,0.1)';
-        context.lineWidth = 0.5;
-        context.stroke();
+        if (chart.drawLinks) {
+          graph.links.forEach(drawLink);
+          context.strokeStyle = 'rgba(200,200,200,0.1)';
+          context.lineWidth = 0.5;
+          context.stroke();
+        }
+
+
 
 
         clusters.forEach(function(cluster) {
@@ -169,48 +190,54 @@ var scrollVis = function() {
 
 
       function drawNode(d) {
-        // context.moveTo(d.x + radius, d.y);
-        // context.arc(d.x, d.y, radius, 0, 2 * Math.PI);
+        if (HIDE_IMAGES) {
+          context.moveTo(d.x + radius, d.y);
+          context.arc(d.x, d.y, radius, 0, 2 * Math.PI);
+        } else {
+
+          context.save();
+
+          // If a node is in this list it will be grayed out
+          if (grayingOutList &&
+            grayingOutList[d.id]!==undefined) {
+            console.log("globalAlpha" + grayedLevel);
+            context.globalAlpha = grayedLevel;
+            console.log("globalAlpha");
+            console.log(context.globalAlpha);
+          } else {
+             context.globalAlpha = 1;
+          }
+          if (grayedOutList &&
+            grayedOutList[d.id]!==undefined) {
+            context.globalAlpha = 0.3; //final value
+          } else {
+             context.globalAlpha = 1;
+          }
 
 
-        context.save();
-        context.beginPath();
-        context.arc(d.x+radius, d.y+radius, radius, 0, Math.PI * 2, true);
-        context.closePath();
-        context.clip();
+          context.beginPath();
+          context.arc(d.x+radius, d.y+radius, radius, 0, Math.PI * 2, true);
+          context.closePath();
+          context.clip();
 
-        context.drawImage(d.nodeImg, d.x, d.y, radius*2, radius*2);
+          context.drawImage(d.nodeImg, d.x, d.y, radius*2, radius*2);
 
-        // context.beginPath();
-        // context.arc(d.x, d.y, radius+10, 0, Math.PI * 2, true);
-        // context.clip();
-        // context.closePath();
-        context.restore();
+          // context.beginPath();
+          // context.arc(d.x, d.y, radius+10, 0, Math.PI * 2, true);
+          // context.clip();
+          // context.closePath();
+          context.restore();
+        }
 
-        // nodeImg = new Image();
-        // nodeImg.src = d.profile_image_url;
-        // nodeImg.onload = function() {
-        //     console.log("loaded" + d.profile_image_url);
-        //     context.save();
-        //     context.beginPath();
-        //     context.arc(radius, radius, radius, 0, Math.PI * 2, true);
-        //     context.closePath();
-        //     context.clip();
 
-        //     context.drawImage(nodeImg, 0, 0, radius*2, radius);
-
-        //     context.beginPath();
-        //     context.arc(d.x, d.y, radius, 0, Math.PI * 2, true);
-        //     context.clip();
-        //     context.closePath();
-        //     context.restore();
-        // };
       }
 
 
 
     });
   }; //chart
+
+  chart.drawLinks = false;
 
 
 
@@ -224,12 +251,12 @@ var scrollVis = function() {
    *  element for each filler word type.
    * @param histData - binned histogram data
    */
-  setupVis = function(data) {
+  var setupVis = function(data) {
 
-
+    d3.select("#nodeCount").text(data.nodes.filter(function (d) { return d.influential===false; }).length )
   };
 
-  loadImages = function(graph) {
+  var setupData = function(graph) {
     graph.nodes.forEach(function (d) {
       d.nodeImg = new Image();
       d.nodeImg.src = d.profile_image_url;
@@ -248,26 +275,34 @@ var scrollVis = function() {
    * the section's index.
    *
    */
-  setupSections = function() {
-    var STEPS = 9;
+  var setupSections = function() {
+    var STEPS = 20;
+
+    var nothingFn = function () {};
     // activateFunctions are called each
     // time the active section changes
     activateFunctions[0] = showTitle;
-    activateFunctions[1] = showFillerTitle;
-    activateFunctions[2] = showNodes;
-    activateFunctions[3] = showFillerTitle;
-    activateFunctions[4] = showFillerTitle;
-    activateFunctions[5] = showFillerTitle;
-    activateFunctions[6] = showFillerTitle;
-    activateFunctions[7] = showFillerTitle;
-    activateFunctions[8] = showFillerTitle;
-    // activateFunctions[2] = showGrid;
-    // activateFunctions[3] = highlightGrid;
-    // activateFunctions[4] = showBar;
-    // activateFunctions[5] = showHistPart;
-    // activateFunctions[6] = showHistAll;
-    // activateFunctions[7] = showCough;
-    // activateFunctions[8] = showHistAll;
+    activateFunctions[1] = showInfluentials;
+    activateFunctions[2] = showAllNodes;
+    activateFunctions[3] = showLinks;
+    activateFunctions[4] = showLinks;
+    activateFunctions[5] = showLinks;
+    activateFunctions[6] = showLinks;
+    // Rank by importance
+    activateFunctions[7] = selectInfluentials;
+    activateFunctions[8] = selectNeighborhood;
+    activateFunctions[9] = rankByImportance;
+
+    activateFunctions[10] = showFillerTitle;
+    activateFunctions[11] = showFillerTitle;
+    activateFunctions[12] = showAllNodes;
+    activateFunctions[13] = showFillerTitle;
+    activateFunctions[14] = showFillerTitle;
+    activateFunctions[15] = showFillerTitle;
+    activateFunctions[16] = showAllNodes;
+    activateFunctions[17] = showFillerTitle;
+    activateFunctions[18] = showFillerTitle;
+    activateFunctions[19] = showFillerTitle;
 
     // updateFunctions are called while
     // in a particular section to update
@@ -278,7 +313,8 @@ var scrollVis = function() {
     for(var i = 0; i < STEPS; i++) {
       updateFunctions[i] = function() {};
     }
-    // updateFunctions[7] = updateCough;
+    updateFunctions[7] = updateGrayed;
+    updateFunctions[8] = updateGrayed;
   };
 
   /**
@@ -296,6 +332,14 @@ var scrollVis = function() {
    *
    */
 
+  function updateNodes(nodes) {
+    simulation.nodes(nodes);
+    clusters = d3.nest()
+      .key(function(d) { return d.cluster; })
+      .entries(nodes)
+      .sort(function(a, b) { return b.values.length - a.values.length; });
+  }
+
   /**
    * showTitle - initial title
    *
@@ -309,8 +353,8 @@ var scrollVis = function() {
     context.clearRect(0, 0, width, height);
     context.save();
 
-    context.font = "30px Arial";
-    context.fillText("ieeevis Influentials",10,50);
+    context.font = "40px Arial";
+    context.fillText("IEEEVIS Influentials",width/2-100,height/2);
 
     context.restore();
   }
@@ -335,9 +379,129 @@ var scrollVis = function() {
   }
 
 
-  function showNodes () {
+  function showInfluentials () {
+    if (!chart.graph) return;
+    context.clearRect(0, 0, width, height);
+    simulation.stop();
+    updateNodes(chart.graph.nodes.filter(function (d) { return d.influential === true; }));
+
+    simulation.force("link", function () {})
+    simulation.force("charge", function () {});
+    simulation.alphaTarget(0.1).restart();
+  }
+
+  function showAllNodes () {
+    if (!chart.graph) return;
+    simulation.stop();
+    chart.drawLinks = false;
+    updateNodes(chart.graph.nodes);
     simulation.force("link", function () {});
+    simulation.force("charge", function () {})
+          .force("x", d3.forceX(width/2).strength(0.03))
+          .force("y", d3.forceY(height/2).strength(0.03))
+
+    simulation.alphaTarget(0.1).restart();
+  }
+
+  function showLinks () {
+    if (!chart.graph) return;
+    simulation.stop();
+    chart.drawLinks = true;
+
+    updateNodes(chart.graph.nodes);
+    simulation.force("link", d3.forceLink().id(function (d) { return d.id; } ).strength(0.6))
+          .force("charge", d3.forceManyBody().strength(-100));
+    simulation.force("link")
+          .links(chart.graph.links);
+    simulation.force("charge", function () {})
+          .force("x", d3.forceX(width/2).strength(0.1))
+          .force("y", d3.forceY(height/2).strength(0.1))
+          // .force("collide", function () {});
+
+    grayedOutList = {};
+    grayingOutList = {};
+
+    simulation.alphaTarget(0.1).restart();
+  }
+
+  function selectInfluentials () {
+    if (!chart.graph) return;
+    simulation.stop();
+    chart.drawLinks = false;
+
+
+    // Remove links
+    simulation.force("link", function () {})
+          .force("charge", function () {});
+    simulation.force("charge", function () {})
+          .force("x", d3.forceX(width/2).strength(0.01))
+          .force("y", d3.forceY(height/2).strength(0.01))
+          // .force("collide", function () {});
+
+    // Gray out all the nodes that are not influentials
+    grayingOutList = {};
+    chart.graph.nodes
+      .filter(function (d) { return d.influential!== true; })
+      .forEach(function (d) { grayingOutList[d.id]=d; });
+    grayedScale.range([1.0, 0.3]);
+
+    simulation.alphaTarget(0.1).restart();
+  }
+
+  function selectNeighborhood() {
+    if (!chart.graph) return;
+    simulation.stop();
+    chart.drawLinks = false;
+
+    // Leave the non influentials in gray
+    grayedOutList = grayingOutList;
+    // Bring back 200 other nodes
+    grayingOutList = {};
+    .filter(function (d) { return d.influential!== true; })
+    .slice(0,200)
+    .forEach(function (d) { grayingOutList[d.id]=d; })
+
+    // grayedScale.range([0.3, 1.0]);
+
+
+    // Remove links
+    simulation.force("link", function () {})
+          .force("charge", function () {});
+    simulation.force("charge", function () {})
+          .force("x", d3.forceX(width/2).strength(0.01))
+          .force("y", d3.forceY(height/2).strength(0.01))
+          // .force("collide", function () {});
+
+    simulation.alphaTarget(0.1).restart();
+  }
+
+  function rankByImportance () {
+    if (!chart.graph) return;
+    simulation.stop();
+    chart.drawLinks = false;
+
+    updateNodes(chart.graph.nodes
+      .filter(function (d) {
+        return d.influential ||
+          grayingOutList.indexOf(d.id) !==-1;
+        }));
+
+    // Remove links
+    simulation.force("link", function () {})
+          .force("charge", function () {});
+    simulation.force("charge", function () {})
+          .force("x", d3.forceX(width/2).strength(0.01))
+          .force("y", d3.forceY(height/2).strength(0.01))
+          // .force("collide", function () {});
+
+    simulation.alphaTarget(0.1).restart();
+  }
+
+  function updateGrayed(progress) {
+
+    grayedLevel = grayedScale(progress);
     simulation.restart();
+    console.log(progress + "," + grayedLevel);
   }
 
   /**
@@ -378,13 +542,19 @@ var scrollVis = function() {
  *
  * @param data - loaded tsv data
  */
-function display(data) {
+var display = function(mGraph) {
+  console.log("Data loaded");
   // create a new plot and
   // display it
   var plot = scrollVis();
   d3.select("#vis")
-    .datum(data)
+    .datum(mGraph)
     .call(plot);
+
+  plot.graph = mGraph;
+
+  console.log("Data loaded");
+  console.log(plot.graph);
 
   // setup scroll functionality
   var scroll = scroller()
@@ -406,7 +576,7 @@ function display(data) {
   scroll.on('progress', function(index, progress){
     plot.update(index, progress);
   });
-}
+};
 
 // load data and display
 d3.json("data/ieeevisNetwork.json", display);
